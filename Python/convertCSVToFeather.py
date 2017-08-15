2f5087839838780db1e41ce768d52fa0b67d0c68
SMHendryx <seanmhendryx@gmail.com>
2017-08-15 14:56:27 -0700

    adding licenses

--- Python/convertCSVToFeather.py
+++ Python/convertCSVToFeather.py
@@ -1,7 +1,22 @@
 # File reads in csv and writes to feather
 # Authored by Sean M. Hendryx while working at the University of Arizona
-# contact: seanmhendryx@email.arizona.edu
-# Add licencse and copyright
+# contact: seanmhendryx@email.arizona.edu //github.com/SMHendryx/binaryClassifierParameterGridSearch
+# Copyright (c)  2017 Sean Hendryx
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+####################################################################################################################################################################################
+
 
 #Example usage:
 #python convertCSVToFeather.py /Users/seanmhendryx/reach_context-balancing/reach features.csv
--- Python/logistic.py
+++ Python/logistic.py
@@ -1,7 +1,22 @@
-# File tests logistic regression parameter settings
+# File runs cross validation using sklearn functions
+#
 # Authored by Sean M. Hendryx while working at the University of Arizona
-# contact: seanmhendryx@email.arizona.edu
-# Add licencse and copyright
+# contact: seanmhendryx@email.arizona.edu https://github.com/SMHendryx/binaryClassifierParameterGridSearch
+# Copyright (c)  2017 Sean Hendryx
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+####################################################################################################################################################################################
 
 
 import sys
@@ -45,6 +60,7 @@ c, r = y.shape
 del r
 y = y.reshape(c,)
 scores = cross_val_score(LR, X, y, cv=10, scoring='f1_micro')
+np.mean(scores)
 
 #as opposed to cross_val_score, cross_validate returns a dict of float arrays of shape=(n_splits,), including 'test_score', 'train_score', 'fit_time', and 'score_time'
 cvScores = cross_validate(LR, X, y, cv = 10, scoring = 'f1_micro')
@@ -60,6 +76,6 @@ X = X.as_matrix()
 cvScores = cross_validate(LR, X, y, cv = 10, scoring = 'f1_micro')
 
 cvScoresTestedByPaper = cross_validate(LR, X, y, groups = df['PMCID'], cv = 10, scoring = 'f1_micro')
-
+np.mean(cvScoresTestedByPaper['test_score'])
 
 
