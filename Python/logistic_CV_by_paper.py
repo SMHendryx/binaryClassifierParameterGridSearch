de504b21e256238b7eb38560ecd50820e6ea7550
SMHendryx <seanmhendryx@gmail.com>
2018-05-01 19:57:15 -0700

    code cleanup

--- Python/logistic_CV_by_paper.py
+++ Python/logistic_CV_by_paper.py
@@ -1,22 +1,7 @@
 # File runs cross validation on binary logistic regression classifier using sklearn functions
 #
 # Authored by Sean M. Hendryx while working at the University of Arizona
-# contact: seanmhendryx@email.arizona.edu https://github.com/SMHendryx/binaryClassifierParameterGridSearch
-# Copyright (c)  2017 Sean Hendryx
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-####################################################################################################################################################################################
+# seanmhendryx@email.arizona.edu 2017 https://github.com/SMHendryx/binaryClassifierParameterGridSearch
 
 
 import sys
@@ -34,6 +19,7 @@ from sklearn.model_selection import cross_validate
 from sklearn import metrics
 from sklearn import preprocessing
 
+
 def cvByPaper(df, LR_tolerance = 0.0001):
     """
     :param df: a pandas dataframe with a column named 'label', which is the response variable; 
@@ -46,22 +32,22 @@ def cvByPaper(df, LR_tolerance = 0.0001):
     paperIDs = pandas.unique(df.PMCID)
 
     #loop through folds (where each paper is a fold):
-    f1Scores_train = np.zeros(len(paperIDs))
-    f1Scores_cv = np.zeros(len(paperIDs))
+    f1_scores_train = np.zeros(len(paperIDs))
+    f1_scores_cv = np.zeros(len(paperIDs))
     i = 0
     for paperID in paperIDs:
         print("Evaluating: ", paperID)
 
         #get Training Set:
-        trainingSet = df.loc[df['PMCID'] != paperID]
+        training_set = df.loc[df['PMCID'] != paperID]
         # test that the testSet has the correct number of rows:
-        #df.loc[df['PMCID'] == paperID].shape[0] == df.shape[0] - trainingSet.shape[0]
+        #df.loc[df['PMCID'] == paperID].shape[0] == df.shape[0] - training_set.shape[0]
         testSet = df.loc[df['PMCID'] == paperID]
 
         #get predictor variable in array (X_train) and response variable(y_train)
-        y_train = trainingSet['label']
-        del trainingSet['label']
-        X_train = trainingSet
+        y_train = training_set['label']
+        del training_set['label']
+        X_train = training_set
         #remove PMCID:
         del X_train['PMCID']
         # set up test data:
@@ -82,33 +68,30 @@ def cvByPaper(df, LR_tolerance = 0.0001):
         
         # Compute predictions on train and test (cv):
         y_train_predicted = LR.predict(X_train)
-        f1Score_train = metrics.f1_score(y_train, y_train_predicted)
+        f1_score_train = metrics.f1_score(y_train, y_train_predicted)
         p = metrics.precision_score(y_train, y_train_predicted)
         r = metrics.recall_score(y_train, y_train_predicted)
         #Store the results:
-        f1Scores_train[i] = f1Score_train
+        f1_scores_train[i] = f1_score_train
 
         y_test_predicted = LR.predict(X_test)
-        f1Score_test = metrics.f1_score(y_test, y_test_predicted)
-        f1Scores_cv[i] = f1Score_test
+        f1_score_test = metrics.f1_score(y_test, y_test_predicted)
+        f1_scores_cv[i] = f1_score_test
         
         print("Training F1")
-        print(f1Score_train)
+        print(f1_score_train)
 
         print("CV F1")
-        print(f1Score_test)
+        print(f1_score_test)
         i += 1
 
-    return(f1Scores_train, f1Scores_cv)
+    return(f1_scores_train, f1_scores_cv)
 
 
 # read in data:
-dir = '/Users/seanmhendryx/Data/context'
-inFile = 'features.feather'
-# set wd:
-os.chdir(dir)
+in_file = '/Users/seanmhendryx/Data/context/features.feather'
 
-df = feather.read_dataframe(inFile)
+df = feather.read_dataframe(in_file)
 
 # Remove Context and Event IDs:
 del df['EvtID']
@@ -119,42 +102,14 @@ del df['CtxID']
 # Run with these hyperperameters:
 #In [5]: clf.best_params_
 #Out[5]: {'C': 0.0001, 'penalty': 'l2'}
-f1Scores_train, f1Scores_cv = cvByPaper(df)
+f1_scores_train, f1_scores_cv = cvByPaper(df)
 # LR_tolerance = 0.01
 
 print("Macro Training Average:")
-print(np.mean(f1Scores_train))
+print(np.mean(f1_scores_train))
 print("\n")
 print("Macro CV Average:")
-print(np.mean(f1Scores_cv))
-
-
-
-
-
-#this is not correct, or at least needs to be tested to ensure it is correct.:
-# compute micro average:
-paperIDs = pandas.unique(df.PMCID)
-N = len(df)
-i = 0
-macroAverage_train = 0.0
-macroAverage_test = 0.0
-for paperID in paperIDs:
-    n_i_test = len(df.loc[df['PMCID'] == paperID])
-    #compute weighted averge chunk:
-    av_w_i_test = (n_i_test/N) * f1Scores_cv[i]
-
-    n_i_train = len(df.loc[df['PMCID'] != paperID])
-    #compute weighted averge chunk:
-    av_w_i_train = (n_i_train/N) * f1Scores_train[i]
-    print("weighted average: ", av_w_i_train)
-
-    macroAverage_train += av_w_i_train
-    macroAverage_test += av_w_i_test
-    i += 1
-
-
-
-
-
+print(np.mean(f1_scores_cv))
+# Macro CV Average:
+# 0.0689176818342
 
